3c2586fdfcd1f431f16d3ee503a4ea88481c7a90
Juan de Frías <juafrlo@gmail.com>
2009-12-01 20:12:47 +0100

    Uploading project

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:password] == APP_CONFIG['password']
+      flash[:notice] = t("controllers.sessions.successfully_logged_in")
+      session[:admin] = true
+      redirect_to home_url
+    else
+      flash.now[:error] = t("controllers.sessions.password_incorrect")
+      render 'new'
+    end
+  end
+  
+  def destroy
+    reset_session
+    redirect_to home_url
+  end
+end
