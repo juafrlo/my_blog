d92f2959d971123c92b1ee928d2aa8a422393664
Juan de Frías <juafrlo@gmail.com>
2010-01-25 21:11:45 +0100

    Fixed URL on email

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -2,7 +2,7 @@ class Notifier < ActionMailer::Base
   def comment_notification(comment)
     setup_email(comment)
     @subject    += I18n.t('comments.email.comment_notification_subject')
-    @url = "http://#{APP_CONFIG['site_url']}/#{post_path(comment.post)}"
+    @url = "http://#{APP_CONFIG['site_url']}#{post_path(comment.post)}"
   end  
   
   protected
