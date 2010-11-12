73ae39238338e3cda1a3bb02b138e6f9a7216343
juafrlo <juafrlo@gmail.com>
2010-11-12 17:29:54 +0100

    Better dateblock for SEO

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -5,8 +5,8 @@ module PostsHelper
   
   def date_block(date) 
     html = "<div class='dateblock'>"
-      html +=  "<span class='dateblock_mon'>#{l(date, :format => 'month')}</span>"
-      html +=  "<span class='dateblock_day'>#{l(date, :format => 'day')}</span>"
+      html +=  "<span class='dateblock_mon'>#{l(date, :format => 'month')} </span>"
+      html +=  "<span class='dateblock_day'>#{l(date, :format => 'day')} </span>"
       html +=  "<span class='dateblock_year'>#{l(date, :format => 'year')}</span>"
     html += "</div>"
   end    
