de40ccd3b2024588ab1c5e912c92e2e798f02ab8
juafrlo <juafrlo@gmail.com>
2011-10-11 19:27:52 +0200

    Testing constants values

--- config/environment.rb
+++ config/environment.rb
@@ -48,6 +48,6 @@ require 'coderay'
 require 'redclothcoderay'
 
 RedclothCoderay.coderay_options :lang => :ruby, :css => :class
-SITE_NAME = "Juan de Frías"
-KEYWORDS = "Juan de Frías, blog, Ruby on Rails, tecnología, desarrollo web ágil"
-DESCRIPTION = "Blog de Juan de Frías sobre desarrollo web ágil, con especial atención a Ruby on Rails"
+SITE_NAME = "Juan de Frias"
+KEYWORDS = "Juan de Frias, blog, Ruby on Rails, tecnologia, desarrollo web agil"
+DESCRIPTION = "Blog de Juan de Frias sobre desarrollo web agil, con especial atención a Ruby on Rails"
