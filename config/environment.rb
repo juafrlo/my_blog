163895b4d2ac70305dec1b69a32628fea1b8f8ad
juafrlo <juafrlo@gmail.com>
2011-10-11 20:12:38 +0200

    Updated to Rails 2.3.11

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
