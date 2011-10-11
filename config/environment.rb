3a0e25b7c7085e2d07949b7c580255f5b2675989
juafrlo <juafrlo@gmail.com>
2011-10-11 19:31:44 +0200

    Testing config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -43,6 +43,3 @@ require 'coderay'
 require 'redclothcoderay'
 
 RedclothCoderay.coderay_options :lang => :ruby, :css => :class
-SITE_NAME = "Juan de Frías"
-KEYWORDS = "Juan de Frías, blog, Ruby on Rails, tecnología, desarrollo web ágil"
-DESCRIPTION = "Blog de Juan de Frías sobre desarrollo web ágil, con especial atención a Ruby on Rails"
