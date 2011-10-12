ce612fada6f46e507fc632f64ddf396328355ba1
juafrlo <juafrlo@gmail.com>
2011-10-12 15:39:42 +0200

    Fixed Tag find by name

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,7 @@
 class TagsController < ApplicationController
   def show
-    @tag = Tag.find_by_name(params[:id].gsub('-',' '))
+    @tag = Tag.find(:first, :conditions => ['LOWER(name) = ?', params[:id].gsub('-',' ')])
+    
     @posts = Post.ordered.active.find_tagged_with(@tag.name).paginate :per_page => 5,
       :page => params[:page]    
     render( :template=> 'posts/index' )    
